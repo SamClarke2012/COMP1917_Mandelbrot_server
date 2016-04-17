e52c233506240af14a8721bc5c2d220417b66527
samclarke2012 <clarke.sam1@gmail.com>
2016-04-18 07:21:11 +1000

    Now serving: 512px white BMPs

--- bmpServer.c
+++ bmpServer.c
@@ -40,9 +40,22 @@ int main( int argc, char** argv){
 #include <assert.h>
 #include <unistd.h>
 
-int waitForConnection (int serverSocket);
-int makeServerSocket (int portno);
-void serveBMP (int socket);
+#define TRUE  1 
+#define FALSE 0
+
+#define RESERVED         0
+#define NUM_COLORS       0
+#define BYTES_PER_PIXEL  3
+#define BITS_PER_PIXEL   (BYTES_PER_PIXEL*8)
+#define NUMBER_PLANES    1
+#define PIX_PER_METRE    2835
+#define MAGIC_NUMBER     0x4d42
+#define NO_COMPRESSION   0
+#define OFFSET           54
+#define DIB_HEADER_PIXEL_WIDTH 40
+ 
+#define PIXEL_WIDTH  512
+#define NUM_PIXELS   (PIXEL_WIDTH * PIXEL_WIDTH)
 
 #define SIMPLE_SERVER_VERSION 1.0
 #define REQUEST_BUFFER_SIZE 1000
@@ -50,8 +63,17 @@ void serveBMP (int socket);
 #define NUMBER_OF_PAGES_TO_SERVE 10
 // after serving this many pages the server will halt
 
+
+int parseRequest( char *request, float *x, float *y, int *z );
+int waitForConnection (int serverSocket);
+int makeServerSocket (int portno);
+void writeHeader ( int socket );
+void serveBMP (int socket, float x, float y, int z);
+
+
 int main (int argc, char *argv[]) {
-      
+   int z;
+   float x, y;
    printf ("************************************\n");
    printf ("Starting simple server %f\n", SIMPLE_SERVER_VERSION);
    printf ("Serving bmps since 2012\n");   
@@ -78,11 +100,12 @@ int main (int argc, char *argv[]) {
       // were we able to read any data from the connection?
             
       // print entire request to the console 
-      printf (" *** Received http request ***\n %s\n", request);
+      // printf (" *** Received http request ***\n %s\n", request);
+      parseRequest( request, &x, &y, &z );
       
       //send the browser a simple html page using http
-      printf (" *** Sending http response ***\n");
-      serveBMP(connectionSocket);
+      // printf (" *** Sending http response ***\n");
+      serveBMP(connectionSocket, x, y, z);
       
       // close the connection after sending the page- keep aust beautiful
       close(connectionSocket);
@@ -91,45 +114,11 @@ int main (int argc, char *argv[]) {
    } 
    
    // close the server connection after we are done- keep aust beautiful
-   printf ("** shutting down the server **\n");
+   // printf ("** shutting down the server **\n");
    close (serverSocket);
    
    return EXIT_SUCCESS; 
 }
-
-void serveBMP (int socket) {
-   char* message;
-   
-   // first send the http response header
-   
-   // (if you write stings one after another like this on separate
-   // lines the c compiler kindly joins them togther for you into
-   // one long string)
-   message = "HTTP/1.0 200 OK\r\n"
-                "Content-Type: image/bmp\r\n"
-                "\r\n";
-   printf ("about to send=> %s\n", message);
-   write (socket, message, strlen (message));
-   
-   // now send the BMP
-   unsigned char bmp[] = {
-     0x42,0x4d,0x5a,0x00,0x00,0x00,0x00,0x00,
-     0x00,0x00,0x36,0x00,0x00,0x00,0x28,0x00,
-     0x00,0x00,0x03,0x00,0x00,0x00,0x03,0x00,
-     0x00,0x00,0x01,0x00,0x18,0x00,0x00,0x00,
-     0x00,0x00,0x24,0x00,0x00,0x00,0x13,0x0b,
-     0x00,0x00,0x13,0x0b,0x00,0x00,0x00,0x00,
-     0x00,0x00,0x00,0x00,0x00,0x00,0x07,0x07,
-     0xff,0x07,0x07,0x07,0x07,0x07,0xff,0x00,
-     0x00,0x0e,0x07,0x07,0x07,0x66,0x07,0x07,
-     0x07,0x07,0x07,0x00,0x00,0x0d,0x07,0x07,
-     0x07,0x07,0x07,0x07,0xff,0xff,0xff,0x00,
-     0x00,0x0d};
-
-
-   write (socket, bmp, sizeof(bmp));
-}
-
 
 // start the server listening on the specified port number
 int makeServerSocket (int portNumber) { 
@@ -192,4 +181,73 @@ int waitForConnection (int serverSocket) {
    // error on accept
    
    return (connectionSocket);
+}
+
+int parseRequest( char *request, float *x, float *y, int *z ){
+  //GET /tile_x-1.0_y-0.2_z9.bmp HTTP/1.1
+  // int rz;
+  // float rx, ry;
+  int r = sscanf(request, "GET /tile_x%e_y%e_z%d.bmp", x, y, z);
+  printf("requested x:%f y:%f z:%d\n\n%d\n",*x,*y,*z, r );
+}
+
+void serveBMP (int socket, float x, float y, int z) {
+   char* message;
+   
+   // first send the http response header
+   
+   // (if you write stings one after another like this on separate
+   // lines the c compiler kindly joins them togther for you into
+   // one long string)
+   message = "HTTP/1.0 200 OK\r\n"
+                "Content-Type: image/bmp\r\n"
+                "\r\n";
+   // printf ("about to send=> %s\n", message);
+   write (socket, message, strlen (message));
+   
+   // now send the BMP
+   unsigned char bmp[NUM_PIXELS*BYTES_PER_PIXEL] = {255};
+
+   writeHeader(socket);
+  unsigned int i;
+  unsigned char white = 255;
+  for(i=0; i<(512*512*3); i++){
+      write(socket, &white, 1);
+  }
+}
+
+
+void writeHeader ( int socket ) {
+    // Header bytes++
+    uint16_t magicNumber = MAGIC_NUMBER;
+    write(socket, &magicNumber, sizeof(uint16_t));
+    uint32_t fileSize = OFFSET+(PIXEL_WIDTH * PIXEL_WIDTH * BYTES_PER_PIXEL);
+    write(socket, &fileSize, sizeof(fileSize));
+    uint32_t reserved = 0;
+    write(socket, &reserved, sizeof(uint32_t));
+    uint32_t offset = OFFSET;
+    write(socket, &offset, sizeof(uint32_t));
+    uint32_t dibHeaderSize = DIB_HEADER_PIXEL_WIDTH;
+    write(socket, &dibHeaderSize, sizeof(uint32_t));
+    uint32_t width = PIXEL_WIDTH;
+    uint32_t height = width;
+    write(socket, &width, sizeof(uint32_t));
+    write(socket, &height, sizeof(uint32_t));
+    uint16_t planes = NUMBER_PLANES;
+    write(socket, &planes, sizeof(uint16_t));
+    uint16_t bitsPerPixel = BITS_PER_PIXEL;
+    write(socket, &bitsPerPixel, sizeof(uint16_t));
+    uint32_t compression = NO_COMPRESSION;
+    write(socket, &compression, sizeof(uint32_t));
+    uint32_t imageSize = (PIXEL_WIDTH * PIXEL_WIDTH * BYTES_PER_PIXEL);
+    write(socket, &imageSize, sizeof(uint32_t));
+    uint32_t hResolution = PIX_PER_METRE;
+    uint32_t vResolution = hResolution;
+    write(socket, &hResolution, sizeof(uint32_t));
+    write(socket, &vResolution, sizeof(uint32_t));
+    uint32_t numColors = NUM_COLORS;
+    uint32_t importantColors = numColors;
+    write(socket, &numColors, sizeof(uint32_t));
+    write(socket, &importantColors, sizeof(uint32_t));
+    // test
 }
